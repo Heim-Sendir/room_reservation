bd0133e2cebb6bdc85eca37a67208a66addd9c14
Heim_Sendir <alex.noa29@gamil.com>
2025-01-23 17:49:24 +0500

    add db

--- app/core/config.py
+++ app/core/config.py
@@ -3,7 +3,7 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
     app_title: str = 'Бронирование переговорок'
-    app_description: str = 'Бронирование переговорок'
+    database_url: str
 
     class Config:
         env_file = '.env'
