c0b5189131c903d1ec89525c10e22133877ec59b
Heim_Sendir <alex.noa29@gamil.com>
2025-01-24 13:00:02 +0500

    add db and apply first migration

--- app/models/meeting_room.py
+++ app/models/meeting_room.py
@@ -1,7 +1,10 @@
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, Integer, Text
 
 from app.core.db import Base
 
 
 class MeetingRoom(Base):
+    __tablename__ = "meeting_rooms"
+    id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(100), unique=True, nullable=False)
+    description = Column(Text)
