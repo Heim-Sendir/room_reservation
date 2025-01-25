f089e6f1bce2c8bf9117764641d72ff51389a3d6
Heim_Sendir <alex.noa29@gamil.com>
2025-01-25 11:28:32 +0500

    add checking an existing object

--- app/crud/meeting_room.py
+++ app/crud/meeting_room.py
@@ -1,3 +1,7 @@
+from typing import Optional
+
+from sqlalchemy import select
+
 from app.core.db import AsyncSessionLocal
 from app.models.meeting_room import MeetingRoom
 from app.schemas.meeting_room import MeetingRoomCreate
@@ -14,3 +18,14 @@ async def create_meeting_room(
         await session.commit()
         await session.refresh(db_room)
     return db_room
+
+
+async def get_room_id_by_name(room_name: str) -> Optional[int]:
+    async with AsyncSessionLocal() as session:
+        db_room_id = await session.execute(
+            select(MeetingRoom.id).where(
+                MeetingRoom.name == room_name
+            )
+        )
+        db_room_id = db_room_id.scalars().first()
+    return db_room_id
